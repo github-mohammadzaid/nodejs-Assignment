ecf1deba0cf2e438bfda8e3f98d87d855c221b04
github-mohammadzaid <119792263+github-mohammadzaid@users.noreply.github.com>
2022-12-22 10:47:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-require("./db/conn");
-const product = require("./models/products");
+require("./conn");
+const product = require("./products");
 const app = express();
 const port = process.env.PORT || 3000;
 
